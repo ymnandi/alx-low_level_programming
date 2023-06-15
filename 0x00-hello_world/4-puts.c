461797d84b546c774c6069b7ecf5477828929e26
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-15 22:03:36 +0300

    C is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main- This the entry point of the program
+* Return: 0 if the program exited properly, otherwise the program failed
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- This is the main entry point of the program.
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
