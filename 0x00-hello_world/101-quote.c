bfbc6edbcd27b45d79cfde591bab27663c3c6a38
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-16 11:00:22 +0300

    piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- Entry point of the program.
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
