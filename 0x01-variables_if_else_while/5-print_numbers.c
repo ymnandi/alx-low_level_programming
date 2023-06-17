6f6698edb73851a689a1cff98f7490aa508b1146
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 12:07:16 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.prints the numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{printf("%d", c);
+}
+printf("\n");
+return (0);
+}
