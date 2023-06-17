ad0006e24ac9fde19957bd3e0ea7dbf70982bd04
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 12:15:30 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.prints number from 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int v;
+for (v = 0; v <= 10; v++)
+{ putchar(v);
+}
+putchar('\n');
+return (0);
+}
