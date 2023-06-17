4a63f686d289961fc5e68b97961cb46a72430ecb
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 13:25:28 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include stdio.h
+/**
+ * main - Entry point.prints alphabet in reverse
+ * Return: Always 0 when (Successful)
+ */
+int main(void)
+{
+int k, v;
+for (k = 0; k <= 9; k++)
+{
+for (v = k; v <= 9; v++)
+{
+putchar('0' + k);
+putchar('0' + v);
+}
+if (k != 9 || v != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+{
+putchar('\n');
+return (0);
+}
+}
