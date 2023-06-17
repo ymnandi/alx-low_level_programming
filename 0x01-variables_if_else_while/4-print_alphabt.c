7870d1daed6dd51222581080a05522f9e3378b1d
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 11:54:57 +0300

    aplabet is fun

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point.prints the alphabet in lowercase using ASCII code
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+for (c = 97; c <= 122; c++)
+{
+if (c != 101 && c != 113)
+putchar(c);
+}
+return (0);
+}
