5a65adc7b659ecd4927b6dc754892d8d20da8b8a
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 12:46:38 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.prints alphabet in reverse
+ * Return: Always 0 when (Successful)
+ */
+int main(void)
+{
+int k;
+for (k = 122; k >= 97; k--)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
