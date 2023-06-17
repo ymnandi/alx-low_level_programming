5059ed6c4445b74db89738528ccf831b4826f64d
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-17 13:49:10 +0300

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 /**
- * main - Entry point.prints alphabet in reverse
+ * main - Entry point.print numebers with commas and spaces
  * Return: Always 0 when (Successful)
  */
 int main(void)
 {
-int k, v;
-for (k = 0; k <= 9; k++)
+int v;
+for (v = 48; v < 57; v++)
 {
-for (v = k; v <= 9; v++)
+putchar(v);
+if (v != 57)
 {
-putchar('0' + k);
-putchar('0' + v);
-}
-if (k != 9 || v != 9)
-{
-putchar(',', ' ');
+putchar(',');
 putchar(' ');
 }
 }
-{
+putchar('\n');
 return (0);
-}
 }
