489eb6b77fcb1a345b4e6cd94d887cf4e3cd5676
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-19 18:51:13 +0300

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 int dig1, dig2, dig3;
-for (dig1 = 0; dig1 <=7; dig1++)
+for (dig1 = 0; dig1 <= 7; dig1++)
 {
 for (dig2 = dig1 + 1; dig2 <= 8; dig2++)
 {
 {
-for (dig3 = dig2 + 1; dig3 <=9; dig3++)
+for (dig3 = dig2 + 1; dig3 <= 9; dig3++)
 {
 putchar(dig1  + '0');
 putchar(dig2  + '0');
