c9865a1a8979873a410a2e346005c25f8367e09d
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-19 19:58:53 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,6 @@
  * description :prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int k, v;
@@ -15,12 +14,13 @@ for (v = k + 1; v <= 9; v++)
 {
 putchar(k + '0');
 putchar(v + '0');
-if (k != 8 || v != 9)
+if (!(k == 8 && v == 9))
 {
 putchar(',');
 putchar(' ');
 }
 }
-return (0);
 }
+putchar('\n');
+return (0);
 }
