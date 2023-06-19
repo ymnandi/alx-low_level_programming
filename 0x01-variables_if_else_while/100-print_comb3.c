4b64c3e48eaf3895d4988559a16c51b44a3864c6
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-19 19:36:19 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description :prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int k, v;
+for (k = 0; k <= 8; k++)
+{
+for (v = k + 1; v <= 9; v++)
+{
+putchar(k + '0');
+putchar(v + '0');
+}
+}
+return (0);
+}
