77f064f39110002df3bd639e6e3e8e984be3de32
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-19 18:48:22 +0300

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: prints all possible combinations of three-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int dig1, dig2, dig3;
+for (dig1 = 0; dig1 <=7; dig1++)
+{
+for (dig2 = dig1 + 1; dig2 <= 8; dig2++)
+{
+{
+for (dig3 = dig2 + 1; dig3 <=9; dig3++)
+{
+putchar(dig1  + '0');
+putchar(dig2  + '0');
+putchar(dig3  + '0');
+if (dig1 != 7 || dig2 != 8 || dig3 != 9)
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
