ef7edd21dbefd51ac75f04fea7b706c2d79a52c0
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-21 11:02:39 +0300

    task 1,2

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int _putchar(char c);
+int print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
