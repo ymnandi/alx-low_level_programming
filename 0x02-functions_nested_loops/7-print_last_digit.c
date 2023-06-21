90aead7558a8bb1c4f9c062df69ddb8cc5572584
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-21 13:57:11 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,15 @@
  */
 int print_last_digit(int n)
 {
-int last_digit = n % 10;
-printf("%d", last_digit);
+int last_digit;
+if (n < 0)
+{
+last_digit = -1 * (n % 10);
+}
+else
+{
+last_digit = n % 10;
+}
+_putchar(last_digit + '0');
 return (last_digit);
 }
