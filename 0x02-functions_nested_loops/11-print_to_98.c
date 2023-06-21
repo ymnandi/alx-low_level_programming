68c0d1bf317a95fb02256e4ab1db92a8c4d4c9a8
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-21 14:17:06 +0300

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,28 @@
  * Description: prints all natural numbers from n to 98
  * Return: Always 0.
  */
-
 void print_to_98(int n)
-{
-printf("%d", n);
-if (n <= 98)
-{
-for (int i = n; i <= 98; i++)
-{
-printf(", %d", i);
-}
-}
-else if (n > 98)
-{
-for (int i = n; i > 98; i--)
-{
-printf(", %d", i);
-}
-}
-printf("\n");
-}
+	{
+		int i, j;
+
+
+		if (n <= 98)
+		{
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+			}
+		} else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
+		}
+	}
