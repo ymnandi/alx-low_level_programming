7423baf4b9c67bf7d47ee5626a8f62499173b7ef
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-21 11:14:11 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int _putchar(char c);
-int print_alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 int add(int, int);
