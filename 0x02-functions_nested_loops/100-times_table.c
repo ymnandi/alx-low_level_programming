7f864ce80c0170c70eef65ba44ac278766d0028c
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-21 22:24:06 +0300

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,30 +7,28 @@
  * Return: void
  */
 void print_times_table(int n)
+{int i, j, k;
+if (n >= 0 && n <= 15)
 {
-if (n < 0 || n > 15)
-return;
-for (int i = 0; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
-for (int j = 0; j <= n; j++)
-{
-int k = i * j;
-if (j != 0)
-_putchar(',');
+for (j = 0; j <= n; j++)
+{k = i * j;
+if (j == 0)
+{_putchar(k + '0'); }
+else if (k <= 9)
+{_putchar(' ');
 _putchar(' ');
-_putchar(' ');
-if (k <= 9)
-_putchar(' ');
-if (k <= 99)
-_putchar(' ');
-if (k >= 100)
-_putchar((k / 100) % 10 + '0');
-if (k >= 10)
+_putchar(k + '0'); }
+else if (k <= 99)
+{_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0'); }
+else
+{_putchar(k / 100 + '0');
 _putchar((k / 10) % 10 + '0');
-_putchar(k % 10 + '0');
+_putchar(k % 10 + '0'); }
 if (j != n)
-_putchar(' ');
-}
-_putchar('\n');
-}
-}
+{_putchar(','); }
+else
+{_putchar('\n'); }}}}}
