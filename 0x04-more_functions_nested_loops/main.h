6702f205519fc865de57182d07cca52a3d895917
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-22 17:32:26 +0300

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void print_number(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+void print_times_table(int n);
+void print_to_98(int n);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+#endif
