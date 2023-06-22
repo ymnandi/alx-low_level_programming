7ab2149b6a27217f7c07ff843ff7ee866a6733ce
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-22 14:25:08 +0300

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - tests function that prints if integer is positive or negative
  *
@@ -11,4 +12,5 @@ int i;
 i = 0;
 positive_or_negative(i);
 return (0);
+
 }
