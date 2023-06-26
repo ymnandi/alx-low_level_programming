24d727c76a2002833b84baba40f2be958a21c900
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-26 17:35:25 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,24 +8,15 @@
  */
 int main(void)
 {
-int password[100];
-int i, sum, n;
-sum = 0;
+char *s;
+int i;
 
-srand(time(NULL));
-
-for (i = 0; i < 100; i++)
+srand(time(0));
+for (i = 0; i < 10; i++)
 {
-password[i] = rand() % 78;
-sum += (password[i] + '0');
-putchar(password[i] + '0');
-if ((2772 - sum)-'0' < 78)
-{
-n = 2772 - sum - '0';
-sum += n;
-putchar(n + '0');
-break;
+s = generate_random_key();
+printf("%s", s);
 }
-}
+printf("\n");
 return (0);
 }
