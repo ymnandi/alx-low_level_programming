a0b465325bccbb2791193bcea9f045be0e0f86f4
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-26 14:50:52 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <time.h>
 
 /**
  * main - check the code
@@ -10,7 +11,9 @@ int main(void)
 int password[100];
 int i, sum, n;
 sum = 0;
+
 srand(time(NULL));
+
 for (i = 0; i < 100; i++)
 {
 password[i] = rand() % 78;
