4a9f186fd133d25967382b22ddea234d4a668050
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-29 20:15:09 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,17 +10,25 @@
 char *leet(char *str)
 {
 int i, j;
-char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-char num[8] = {'0', '1', '3', '4', '5', '6', '7', '7'};
-i = 0;
-while (*(str + i))
+char leet[] = "4433007711";
+char alpha[] = "aAeEoOtTlL";
+for (i = 0; str[i]; i++)
 {
-for (j = 0; j < 8; j++)
+for (j = 0; alpha[j]; j++)
 {
-if (*(str + i) == leet[j] || *(str + i) == (leet[j] + 32))
-*(str + i) = num[j];
+if (str[i] == alpha[j])
+str[i] = leet[j];
 }
-i++;
 }
 return (str);
+}
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
