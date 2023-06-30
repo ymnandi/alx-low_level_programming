41b389a2013f8126f764ce86d4a34775895038bd
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-06-30 05:18:41 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,30 +7,21 @@
  *
  * Return: void
  */
-
 void print_number(int n)
 {
-unsigned int num, num2;
-int i, d, num3;
-if (n < 0)
-{
-_putchar('-');
-num = -n;
-}
-else
-{
-num = n;
-}
-num2 = num;
-d = 1;
-while (num2 > 9)
-{
-d *= 10;
-num2 /= 10;
-}
-for (i = d; i >= 1; i /= 10)
-{
-num3 = num / i;
-_putchar((num3 % 10) + '0');
-}
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
