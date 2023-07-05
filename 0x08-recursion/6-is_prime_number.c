ac4fd3974c3a3dcff4f8177415d0fab073a4f4e7
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-07-05 22:47:14 +0300

    coderanger

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,18 +5,6 @@
  * @n: number to check
  * Return: 1 if prime, 0 if not
  */
-int is_prime_number(int n)
-{
-if (n <= 1)
-return (0);
-return (check_prime(n, 2));
-}
-/**
- * check_prime - checks if a number is prime
- * @n: number to check
- * @i: iterator
- * Return: 1 if prime, 0 if not
- */
 int check_prime(int n, int i)
 {
 if (n % i == 0 && i != n)
@@ -24,4 +12,15 @@ return (0);
 if (i == n)
 return (1);
 return (check_prime(n, i + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (check_prime(n, 2));
 }
