4b95e5140f4d700753eb15d14fc68cf2d13acdcb
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-07-05 22:36:14 +0300

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,3 +11,17 @@ if (n < 0)
 return (-1);
 return (find_sqrt(n, 0));
 }
+/**
+ * find_sqrt - finds the square root of n
+ * @n: number to find square root of
+ * @i: iterator
+ * Return: square root of n
+ */
+int find_sqrt(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (find_sqrt(n, i + 1));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,3 +11,17 @@ if (n <= 1)
 return (0);
 return (check_prime(n, 2));
 }
+/**
+ * check_prime - checks if a number is prime
+ * @n: number to check
+ * @i: iterator
+ * Return: 1 if prime, 0 if not
+ */
+int check_prime(int n, int i)
+{
+if (n % i == 0 && i != n)
+return (0);
+if (i == n)
+return (1);
+return (check_prime(n, i + 1));
+}
