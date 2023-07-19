37817bdf46b11626f09079bc4c0de20c19f57715
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-07-19 21:05:58 +0300

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
 /**
- * main - performs simple operations
+ * main - prints the opcodes of its own main function
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 if successful, 98, 99, or 100 if failure
+ * Return: 0 on success, 1 on argc != 2, 2 on negative bytes
  */
 int main(int argc, char *argv[])
 {
-int b, result;
-char *op;
+int i, bytes;
+char *main_ptr;
+
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-b = atoi(argv[1]);
-if (b < 0)
+bytes = atoi(argv[1]);
+if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-op = (char *)main;
-for (result = 0; result < b; result++)
-printf("%02hhx ", op[result]);
+main_ptr = (char *)main;
+for (i = 0; i < bytes; i++)
+{
+printf("%02hhx", main_ptr[i]);
+if (i < bytes - 1)
+printf(" ");
+}
 printf("\n");
 return (0);
 }
