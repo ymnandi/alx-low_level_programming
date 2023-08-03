7660a91016aae5e9cd290b48ea8336740c9ff87f
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-08-03 20:41:32 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,19 +11,28 @@
 
 void print_binary(unsigned long int n)
 {
-unsigned long int i, check;
-int flag = 0;
-if (n == 0)
-_putchar('0');
+int i, count = 0;
+unsigned long int check;
 for (i = 1UL << 63; i > 0; i = i >> 1)
 {
 check = n & i;
-if (check)
+if (check == i)
 {
-_putchar('1');
-flag = 1;
+count++;
+break;
 }
-else if (flag == 1 || i == 1)
+}
+if (count == 0)
 _putchar('0');
+else
+{
+for (i = 1UL << 63; i > 0; i = i >> 1)
+{
+check = n & i;
+if (check == i)
+_putchar('1');
+else if (check == 0)
+_putchar('0');
+}
 }
 }
