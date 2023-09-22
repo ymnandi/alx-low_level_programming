33389b65c526b51760b3d114ce0b83730355263c
youngmayodinandi@gmail.com <ymayodinandi@gmail.com>
2023-09-22 13:41:51 +0300

    1-memcpy.c

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -12,7 +12,14 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+char *dest_ptr = dest;
+char *src_ptr = src;
 for (i = 0; i < n; i++)
-dest[i] = src[i];
+{
+*dest_ptr = *src_ptr;
+dest_ptr++;
+src_ptr++;
+}
 return (dest);
 }
+
